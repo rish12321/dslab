8f0ce85d8fffb1183f11f450e414f22c51578729
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-11-28 22:57:22 +0530

    Create kmeans.R

---
+++ kmeans.R
@@ -0,0 +1,36 @@
+#install pre-requisites packages
+install.packages("stats")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("ggfortify")
+
+#load required libraries
+library(stats)
+library(dplyr)
+library(ggplot2)
+library(ggfortify)
+View(iris)
+
+mydata = select(iris,c(1,2,3,4))
+
+#wss plot to choose maximum number of clusters
+wssplot(mydata)
+
+#K-Means cluster
+KM = kmeans(mydata,2)
+
+#cluster plot
+autoplot(KM,mydata,frame=TRUE)
+
+#cluster centres
+KM$centers
+
+#wss plot function
+wssplot <- function(data, nc=15, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Within groups sum of squares")
+}
