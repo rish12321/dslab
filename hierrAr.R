773d868d88c06e5f9f8562e33681e999e3b3cb45
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-11-28 23:14:12 +0530

    Create hierrAr.R

---
+++ hierrAr.R
@@ -0,0 +1,14 @@
+install.packages("dplyr")
+library(dplyr)
+head(mtcars)
+distance_mat <- dist(mtcars, method = 'euclidean')
+distance_mat
+set.seed(240)  # Setting seed
+Hierar_cl <- hclust(distance_mat, method = "average")
+Hierar_cl
+plot(Hierar_cl)
+abline(h = 110, col = "green")
+fit <- cutree(Hierar_cl, k = 3 )
+fit
+table(fit)
+rect.hclust(Hierar_cl, k = 3, border = "green")
