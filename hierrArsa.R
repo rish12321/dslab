4b8e16953532cd8757e64cfa2e6a5bdfe0fccdbd
Raushan Ranjan <74482501+raushanranjan103@users.noreply.github.com>
2022-11-29 10:29:21 +0530

    Create hierrArsa.R

---
+++ hierrArsa.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+
+install.packages("dplyr")
+
+
+library(dplyr)
+
+
+head(mtcars)
+
+distance_mat <- dist(mtcars, method = "euclidean")
+distance_mat
+
+
+set.seed(240)
+
+Hierar_cl <- hclust(distance_mat, method = "average")
+Hierar_cl
+
+plot(Hierar_cl)
+
+
+abline(h = 110, col = "green")
+
+fit <- cutree(Hierar_cl, k = 3)
+fit
+
+table(fit)
+rect.hclust(Hierar_cl, k = 3, border = "green")
