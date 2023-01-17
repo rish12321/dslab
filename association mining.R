a5a0655e9d05d533a0b95090adc6fea6137471b7
rish12321 <53363078+rish12321@users.noreply.github.com>
2023-01-17 23:19:39 +0530

    Update association mining.R

--- association mining.R
+++ association mining.R
@@ -1,28 +1,20 @@
 # Loading data
 setwd("C:/Users/raush/Downloads")  
 dataset = read.transactions('Market_Basket_Optimisation.csv',sep = ',', rm.duplicates = TRUE)
-
-# Structure
 str(dataset)
-# Installing Packages
+
 install.packages("arules")
 install.packages("arulesViz")
 
-# Loading package
 library(arules)
 library(arulesViz)
 
-# Fitting model
-# Training Apriori on the dataset
+# Fitting model--Training Apriori on the dataset
 set.seed = 220 # Setting seed
-associa_rules = apriori(data = dataset,
-						parameter = list(support = 0.004,
-										confidence = 0.2))
+associa_rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
 
-# Plot
-itemFrequencyPlot(dataset, topN = 10)
+itemFrequencyPlot(dataset, topN = 10) #Plot
 
 # Visualising the results
 inspect(sort(associa_rules, by = 'lift')[1:10])
-plot(associa_rules, method = "graph",
-	measure = "confidence", shading = "lift")
+plot(associa_rules, method = "graph", measure = "confidence", shading = "lift")
