2fe03769962b54ca0ba6946fbf7bc1ecafdf0279
rish12321 <53363078+rish12321@users.noreply.github.com>
2023-01-17 23:03:49 +0530

    Update logisticgfg.R

--- logisticgfg.R
+++ logisticgfg.R
@@ -1,21 +1,14 @@
-# Installing the package
 install.packages("dplyr")
-
-# Loading package
 library(dplyr)
 
-# Summary of dataset in package
 summary(mtcars)
 
-# Installing the package
-install.packages("caTools") # For Logistic regression
+install.packages("caTools")      #For Logistic regression
 install.packages("ROCR")	 # For ROC curve to evaluate model
 	
-# Loading package
 library(caTools)
 library(ROCR)
 
-# Splitting dataset
 split <- sample.split(mtcars, SplitRatio = 0.8)
 split
 
@@ -23,17 +16,13 @@ train_reg <- subset(mtcars, split == "TRUE")
 test_reg <- subset(mtcars, split == "FALSE")
 
 # Training model
-logistic_model <- glm(vs ~ wt + disp,
-					data = train_reg,
-					family = "binomial")
+logistic_model <- glm(vs ~ wt + disp, data = train_reg, family = "binomial")
 logistic_model
 
-# Summary
 summary(logistic_model)
 
 # Predict test data based on model
-predict_reg <- predict(logistic_model,
-					test_reg, type = "response")
+predict_reg <- predict(logistic_model,test_reg, type = "response")
 predict_reg
 
 # Changing probabilities
@@ -48,8 +37,7 @@ print(paste('Accuracy =', 1 - missing_classerr))
 
 # ROC-AUC Curve
 ROCPred <- prediction(predict_reg, test_reg$vs)
-ROCPer <- performance(ROCPred, measure = "tpr",
-							x.measure = "fpr")
+ROCPer <- performance(ROCPred, measure = "tpr", x.measure = "fpr")
 
 auc <- performance(ROCPred, measure = "auc")
 auc <- auc@y.values[[1]]
@@ -57,9 +45,7 @@ auc
 
 # Plotting curve
 plot(ROCPer)
-plot(ROCPer, colorize = TRUE,
-	print.cutoffs.at = seq(0.1, by = 0.1),
-	main = "ROC CURVE")
+plot(ROCPer, colorize = TRUE, print.cutoffs.at = seq(0.1, by = 0.1), main = "ROC CURVE")
 abline(a = 0, b = 1)
 
 auc <- round(auc, 4)
