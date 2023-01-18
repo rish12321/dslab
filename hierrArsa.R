0ab22c06fc86607db2c43cce10af094f8c0f2b96
rish12321 <53363078+rish12321@users.noreply.github.com>
2023-01-18 13:12:12 +0530

    Update hierrArsa.R

--- hierrArsa.R
+++ hierrArsa.R
@@ -1,17 +1,12 @@
 #!/usr/bin/env Rscript
 
-
 install.packages("dplyr")
-
-
 library(dplyr)
-
 
 head(mtcars)
 
 distance_mat <- dist(mtcars, method = "euclidean")
 distance_mat
-
 
 set.seed(240)
 
@@ -19,7 +14,6 @@ Hierar_cl <- hclust(distance_mat, method = "average")
 Hierar_cl
 
 plot(Hierar_cl)
-
 
 abline(h = 110, col = "green")
 
